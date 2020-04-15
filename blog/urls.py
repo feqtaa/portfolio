1c25eec087ba1fe003ebbaf456e7b03d5b80d630
Pavlina Miroslavova Dimitrova <pavdimittrova@gmail.com>
2020-04-15 16:28:25 +0100

    Added blog page and blogs, blog details now showing

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+import jobs.views
+from . import views
+
+urlpatterns = [
+                  path('', views.allblogs, name='blog/allblogs'),
+
+              ]
